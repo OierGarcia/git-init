fe79b986a37d03bff1b44fdf1b7372c8aa3b6983
Oier Garcia <garcia.oier@uni.eus>
2026-09-17 13:46:08 +0200

    feat: hizkuntza aukera gehitu agur funtzioan

--- agur.py
+++ agur.py
@@ -1,5 +1,11 @@
-def agur(izena="Lagun"):
-    print(f"Kaixo, {izena}!")
+def agur(izena="Lagun", hizkuntza="eu"):
+    if hizkuntza == "es":
+        print(f"¡Hola, {izena}!")
+    elif hizkuntza == "en":
+        print(f"Hello, {izena}!")
+    else:
+        print(f"Kaixo, {izena}!")
 
 if __name__ == "__main__":
-    agur("Oier")
+    agur("Oier", "eu")
+    agur("Mikel", "es")
