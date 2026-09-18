c60cc43b1e243cf125e4149753fcd66781c452fc
UnaiMateos <mateos.unai@uni.eus>
2026-09-18 08:22:36 +0200

    feat: kaixo joseba geitu dugu

--- agur.py
+++ agur.py
@@ -6,6 +6,8 @@ def agur(izena="Lagun", hizkuntza="eu"):
     else:
         print(f"Kaixo, {izena}!")
 
+
 if __name__ == "__main__":
     agur("Oier", "eu")
     agur("Mikel", "es")
+    agur("Joseba", "en")
